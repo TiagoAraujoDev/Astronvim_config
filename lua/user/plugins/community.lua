e29128c2eda96328125e90075eb35ad4f15dbf14
Tiago Muniz de Araujo <102497603+TiagoAraujoDev@users.noreply.github.com>
2023-06-13 23:15:16 +0100

    feat(todo-comments): Add keymaps to find todos

--- lua/user/plugins/community.lua
+++ lua/user/plugins/community.lua
@@ -17,6 +17,11 @@ return {
   { import = "astrocommunity.utility.transparent-nvim" },
   { import = "astrocommunity.editing-support.nvim-ts-rainbow" },
   { import = "astrocommunity.editing-support.todo-comments-nvim" },
+  { "todo-comments.nvim",
+    keys = {
+      { "<leader>fT", "<cmd>TodoTelescope<cr>", desc = "Find ToDo Comments" }
+    }
+  },
   { import = "astrocommunity.motion.nvim-surround" },
   { import = "astrocommunity.markdown-and-latex.glow-nvim" },
   { "glow.nvim",
