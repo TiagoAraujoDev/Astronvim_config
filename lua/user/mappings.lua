1f935def37c46cc6e3a5c47db086cd3b6280433f
Tiago Muniz de Araujo <102497603+TiagoAraujoDev@users.noreply.github.com>
2023-06-13 23:34:07 +0100

    refactor: Cleanup mappings table

--- lua/user/mappings.lua
+++ lua/user/mappings.lua
@@ -8,37 +8,15 @@ return {
   n = {
     -- second key is the lefthand side of the map
     -- mappings seen under group name "buffer"
-    ["<leader>bn"] = { "<cmd>tabnew<cr>", desc = "new tab" },
-    ["<leader>bd"] = {
-      function()
-        require("astronvim.utils.status").heirline.buffer_picker(
-          function(bufnr) require("astronvim.utils.buffer").close(bufnr) end
-        )
-      end,
-      desc = "pick to close",
-    },
-    ["<leader>b"] = { name = "buffers" },
-    ["<leader>fp"] = { "<cmd>Telescope projects initial_mode=normal<cr>", desc = "find projects" },
-
     -- switch buffer
-    ["<S-h>"] = { "<plug>(CybuPrev)", desc = "next buffer" },
-    ["<S-l>"] = { "<plug>(CybuNext)", desc = "previous buffer" },
     -- noice close notification
     ["<leader>n"] = { "<cmd>lua require('notify').dismiss()<cr>", desc = "dismiss notification" },
 
     -- run libgdx
     ["<leader><f4>"] = { "<cmd>!./gradlew run<cr>", desc = "run libgdx" },
-
-    -- -- telescope
-    --
-    --  ["<leader>fT"] = { "<cmd>TodoTelescope<cr>", desc = "Todo Comments" },
   },
   v = {
     -- Better paste
     ["p"] = { '"_dP', desc = "Better paste" },
-  },
-  t = {
-    -- setting a mapping to false will disable it
-    -- ["<esc>"] = false,
   },
 }
