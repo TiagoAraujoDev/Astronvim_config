4c9d92f707154a152b6cc9a5137b54c57c387ba8
Tiago Muniz de Araujo <102497603+TiagoAraujoDev@users.noreply.github.com>
2023-06-13 23:28:19 +0100

    refactor(astrocommunity): Organize config and imports

--- lua/user/plugins/community.lua
+++ lua/user/plugins/community.lua
@@ -15,41 +15,58 @@ return {
   -- Utilities
   { import = "astrocommunity.utility.noice-nvim" },
   { import = "astrocommunity.utility.transparent-nvim" },
+
+  -- Editing support
   { import = "astrocommunity.editing-support.nvim-ts-rainbow" },
   { import = "astrocommunity.editing-support.todo-comments-nvim" },
-  { "todo-comments.nvim",
-    keys = {
-      { "<leader>fT", "<cmd>TodoTelescope<cr>", desc = "Find ToDo Comments" }
-    }
-  },
+
+  -- Motion
   { import = "astrocommunity.motion.nvim-surround" },
+
+  -- Markdown and latex
   { import = "astrocommunity.markdown-and-latex.glow-nvim" },
-  { "glow.nvim",
-    keys = {
-      { "<leader>m", "<cmd>Glow<cr>", desc = "Preview markdown" }
-    }
-  },
 
   -- Project
   { import = "astrocommunity.project.project-nvim" },
   { import = "astrocommunity.project.nvim-spectre" },
 
-  -- Themes
-  { import = "astrocommunity.colorscheme.catppuccin", enabled = false },
-  { import = "astrocommunity.colorscheme.kanagawa-nvim", enabled = true },
-  { import = "astrocommunity.colorscheme.oxocarbon-nvim", enabled = false },
-  { import = "astrocommunity.colorscheme.tokyonight-nvim", enabled = false },
-  { import = "astrocommunity.colorscheme.gruvbox-nvim", enabled = false },
-  { import = "astrocommunity.colorscheme.everforest-nvim", enabled = false },
-  { import = "astrocommunity.colorscheme.nightfox-nvim", enabled = true },
-  { import = "astrocommunity.colorscheme.onigiri-nvim", enabled = false },
-  { import = "astrocommunity.colorscheme.vscode-nvim", enabled = true },
-  { import = "astrocommunity.colorscheme.onedarkpro-nvim", enabled = true },
-  { "onedarkpro.nvim",
+  -- Colorscheme
+  { import = "astrocommunity.colorscheme.catppuccin",            enabled = false },
+  { import = "astrocommunity.colorscheme.kanagawa-nvim",         enabled = true },
+  { import = "astrocommunity.colorscheme.oxocarbon-nvim",        enabled = false },
+  { import = "astrocommunity.colorscheme.tokyonight-nvim",       enabled = false },
+  { import = "astrocommunity.colorscheme.gruvbox-nvim",          enabled = false },
+  { import = "astrocommunity.colorscheme.everforest-nvim",       enabled = false },
+  { import = "astrocommunity.colorscheme.nightfox-nvim",         enabled = true },
+  { import = "astrocommunity.colorscheme.onigiri-nvim",          enabled = false },
+  { import = "astrocommunity.colorscheme.vscode-nvim",           enabled = true },
+  { import = "astrocommunity.colorscheme.onedarkpro-nvim",       enabled = true },
+
+  -- Extra configuration for the community plugins
+  {
+    "todo-comments.nvim",
+    keys = {
+      { "<leader>fT", "<cmd>TodoTelescope<cr>", desc = "Find ToDo Comments" },
+    },
+  },
+  {
+    "onedarkpro.nvim",
     opts = {
       options = {
         highlight_inactive_windows = false,
       },
+    },
+  },
+  {
+    "project.nvim",
+    keys = {
+      { "<leader>fp", "<cmd>Telescope projects initial_mode=normal<cr>", desc = "Find projects" },
+    },
+  },
+  {
+    "glow.nvim",
+    keys = {
+      { "<leader>m", "<cmd>Glow<cr>", desc = "Preview markdown" },
     },
   },
 }
