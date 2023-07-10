ebafeff8246f5624e41f1aef4ce93d1b11526aba
Tiago Muniz de Araujo <102497603+TiagoAraujoDev@users.noreply.github.com>
2023-07-10 12:14:32 +0100

    refactor(mappings): cleanup unnecessary comments

--- lua/user/mappings.lua
+++ lua/user/mappings.lua
@@ -1,17 +1,6 @@
--- Mapping data with "desc" stored directly by vim.keymap.set().
-
--- Please use this mappings table to set keyboard mapping since this is the
--- lower level configuration and more robust one. (which-key will
--- automatically pick-up stored data by this setting.)
-
 return {
-  -- first key is the mode
   n = {
-    -- second key is the lefthand side of the map
-    -- mappings seen under group name "buffer"
     ["<leader>a"] = { "<cmd>enew<cr>", desc = "new file" },
-    -- switch buffer
-    -- noice close notification
     ["<leader>n"] = { "<cmd>lua require('notify').dismiss()<cr>", desc = "dismiss notification" },
 
     -- run libgdx
